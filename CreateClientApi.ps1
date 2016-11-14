4e1c9645b0d10df75646154f337ba369eddf22fa
zijianhuang <zijianhuang2000@gmail.com>
2016-11-14 15:13:09 +1000

    compile ts after generating

--- CreateClientApi.ps1
+++ CreateClientApi.ps1
@@ -5,4 +5,11 @@ echo $arguments
 $processInfo = New-Object System.Diagnostics.ProcessStartInfo("C:\Program Files (x86)\IIS Express\iisexpress.exe", $arguments)
 $process = [System.Diagnostics.Process]::Start($processInfo)
 Invoke-RestMethod http://localhost:10965/api/codegen -Method POST -InFile "$($PSScriptRoot)\DemoWebApi\CodeGen.json" -ContentType "application/json"
+
+$processInfo2 = New-Object System.Diagnostics.ProcessStartInfo("C:\Program Files (x86)\Microsoft SDKs\TypeScript\2.0\tsc.exe", "$PSScriptRoot\DemoWebApi\Scripts\ClientApi\WebApiClientAuto.ts")
+$process2 = [System.Diagnostics.Process]::Start($processInfo2)
+
+$processInfo2 = New-Object System.Diagnostics.ProcessStartInfo("C:\Program Files (x86)\Microsoft SDKs\TypeScript\2.0\tsc.exe", "$PSScriptRoot\DemoAngular2\clientapi\WebApiNG2ClientAuto.ts")
+$process2 = [System.Diagnostics.Process]::Start($processInfo2)
+
 $process.CloseMainWindow()
--- DemoAngular2/clientapi/WebApiNG2ClientAuto.ts
+++ DemoAngular2/clientapi/WebApiNG2ClientAuto.ts
@@ -3,6 +3,19 @@ import { Http, Headers } from '@angular/http'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import { Observable } from 'rxjs/Observable'
+export namespace DemoWebApi_Controllers_Client {
+    export interface FileResult {
+        fileNames?: Array<string>;
+        submitter?: string;
+    }
+
+    export interface Hero {
+        id?: number;
+        name?: string;
+    }
+
+}
+
 export namespace DemoWebApi_DemoData_Client {
     export enum AddressType {Postal, Residential}
 
@@ -61,19 +74,6 @@ export namespace DemoWebApi_DemoData_Another_Client {
     export interface MyPoint {
         x: number;
         y: number;
-    }
-
-}
-
-export namespace DemoWebApi_Controllers_Client {
-    export interface FileResult {
-        fileNames?: Array<string>;
-        submitter?: string;
-    }
-
-    export interface Hero {
-        id?: number;
-        name?: string;
     }
 
 }

b892c37e0000f4456d376cbbd586841663b56828
zijianhuang <admin@fonlow.com>
2017-06-02 07:52:28 +1000

    update packages

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Fonlow")]
-[assembly: AssemblyCopyright("Copyright © Fonlow 2015")]
+[assembly: AssemblyCopyright("Copyright © Fonlow 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

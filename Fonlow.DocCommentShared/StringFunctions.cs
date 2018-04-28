648e5581bd6c3151bde12cbb4ed79a3770460bc6
Z <zijianhuang2000@gmail.com>
2018-04-28 17:13:21 +1000

    .net core for docComment and TS codeDOM

---
+++ Fonlow.DocCommentShared/StringFunctions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fonlow.DocComment
+{
+    public static class StringFunctions
+    {
+        public static string TrimIndentsOfMultiLineText(string s)
+        {
+            var noIndent = TrimIndentedMultiLineTextToArray(s);
+            if (noIndent == null)
+                return null;
+
+            if (noIndent.Length == 1)
+            {
+                return noIndent[0];
+            }
+
+            return String.Join(Environment.NewLine, noIndent).Trim('\r', '\n');
+        }
+
+        public static string[] TrimIndentedMultiLineTextToArray(string s)
+        {
+            if (s == null)
+                return null;
+
+            if (String.IsNullOrWhiteSpace(s))
+                return null;
+
+            var ss = s.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+            var noIndent = ss.Select(d => d.Trim()).ToArray();
+            return noIndent;
+        }
+
+        public static IList<string> TrimIndentsOfArray(IEnumerable<string> ss)
+        {
+            if (ss == null)
+                return null;
+
+            List<string> list = new List<string>();
+            foreach (var item in ss)
+            {
+                var ar = TrimIndentedMultiLineTextToArray(item);
+                if (ar != null)
+                    list.AddRange(ar);
+            }
+
+            return list;
+        }
+
+        public static IList<string> TrimTrimIndentsOfArray(IEnumerable<string> ss)
+        {
+            var list = TrimIndentsOfArray(ss);
+            if (list == null)
+                return null;
+
+            if (list[0] == String.Empty)
+                list.RemoveAt(0);
+
+            if (list[list.Count - 1] == String.Empty)
+            {
+                list.RemoveAt(list.Count - 1);
+            }
+
+            return list;
+        }
+
+        public static string IndentedArrayToString(IEnumerable<string> ss)
+        {
+            if (ss == null)
+                return null;
+
+            return String.Join(Environment.NewLine, TrimIndentsOfArray(ss));
+        }
+    }
+}
---
+++ Fonlow.TypeScriptCodeDomShared/TypeMapper.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fonlow.TypeScriptCodeDom
+{
+    /// <summary>
+    /// Map CLR types to TS types
+    /// </summary>
+    public static class TypeMapper
+    {
+        static readonly Dictionary<string, string> typeMap = new Dictionary<string, string>()
+        {
+            {typeof(int).FullName, "number"},
+            {typeof(uint).FullName, "number"},
+            {typeof(long).FullName, "number"},
+            {typeof(ulong).FullName, "number"},
+            {typeof(short).FullName, "number"},
+            {typeof(ushort).FullName, "number"},
+            {typeof(float).FullName, "number"},
+            {typeof(double).FullName, "number"},
+            {typeof(decimal).FullName, "number"},
+            {typeof(byte).FullName, "number"},
+            {typeof(sbyte).FullName, "number"},
+            {typeof(string).FullName, "string"},
+            {typeof(char).FullName, "string"},
+            {typeof(Guid).FullName, "string"},
+            {typeof(bool).FullName, "boolean"},
+            {typeof(void).FullName, "void"},
+            {typeof(object).FullName, "any"},
+            {typeof(DateTime).FullName, "Date"},
+            {typeof(DateTimeOffset).FullName, "Date"},
+            {typeof(Uri).FullName, "string"},
+            //TimeSpan is not so supported in Javascript
+        };
+
+
+        static bool IsArrayType(CodeTypeReference codeTypeReference)
+        {
+            return codeTypeReference.ArrayElementType != null;
+        }
+
+        static bool IsKeyValuePairType(string typeName)
+        {
+            return typeName.Contains("System.Collections.Generic.KeyValuePair");
+        }
+
+        static readonly string typeNameOfObject = typeof(object).FullName;
+
+        internal static bool IsValidTypeForDerivation(CodeTypeReference type)
+        {
+            return !type.BaseType.Equals(typeNameOfObject);
+        }
+
+        public static string MapToTsBasicType(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+            string tsTypeName;
+
+            if (typeMap.TryGetValue(type.FullName, out tsTypeName))
+                return tsTypeName;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Get the TypeScript text of the CodeTypeReference
+        /// </summary>
+        /// <param name="codeTypeReference">CodeTypeReference to a CLR type</param>
+        /// <returns></returns>
+        public static string MapCodeTypeReferenceToTsText(CodeTypeReference codeTypeReference)
+        {
+            if (codeTypeReference == null)
+                return null;
+
+            System.Diagnostics.Debug.WriteLine($"parameter.type.BaseType: {codeTypeReference.BaseType}  ArgumentCount: {codeTypeReference.TypeArguments.Count}, ArrayRank: {codeTypeReference.ArrayRank}");
+            System.Diagnostics.Debug.WriteLineIf(codeTypeReference.BaseType == "System.Void", "For this void type :" + codeTypeReference.ToString());
+            if (IsArrayType(codeTypeReference))
+            {
+                var rank = codeTypeReference.ArrayRank;
+                if (rank > 1)
+                {
+                    return codeTypeReference.BaseType + new System.Text.StringBuilder().Insert(0, "[]", rank).ToString();
+                }
+                var elementTypeName = MapCodeTypeReferenceToTsText(codeTypeReference.ArrayElementType);
+                return $"Array<{elementTypeName}>";
+            }
+
+            string tsTypeName;
+            if (typeMap.TryGetValue(codeTypeReference.BaseType, out tsTypeName))
+                return tsTypeName;
+
+
+            if (codeTypeReference.TypeArguments.Count == 0)
+            {
+                var codeSnipetTypeReference = codeTypeReference as CodeSnipetTypeReference;
+                if (codeSnipetTypeReference!=null)
+                {
+                    return codeSnipetTypeReference.BaseType;
+                }
+
+                return codeTypeReference.BaseType;
+            }
+
+            if (IsKeyValuePairType(codeTypeReference.BaseType))
+            {
+                System.Diagnostics.Debug.Assert(codeTypeReference.TypeArguments.Count == 2);
+                var keyTypeReferenceText = MapCodeTypeReferenceToTsText(codeTypeReference.TypeArguments[0]);
+                var valueTypeReferenceText = MapCodeTypeReferenceToTsText(codeTypeReference.TypeArguments[1]);
+                return TsCodeGenerationOptions.Instance.CamelCase ? 
+                    $"{{key: {keyTypeReferenceText}, value: {valueTypeReferenceText} }}" 
+                    : $"{{Key: {keyTypeReferenceText}, Value: {valueTypeReferenceText} }}";
+            }
+
+            if (codeTypeReference.BaseType.Contains("System.Collections.Generic.Dictionary"))
+            {
+                System.Diagnostics.Debug.Assert(codeTypeReference.TypeArguments.Count == 2);
+                var keyTypeReferenceText = MapCodeTypeReferenceToTsText(codeTypeReference.TypeArguments[0]);
+                var valueTypeReferenceText = MapCodeTypeReferenceToTsText(codeTypeReference.TypeArguments[1]);
+                return TsCodeGenerationOptions.Instance.CamelCase ?
+                    $"{{[id: {keyTypeReferenceText}]: {valueTypeReferenceText} }}"
+                    : $"{{[Id: {keyTypeReferenceText}]: {valueTypeReferenceText} }}";
+            }
+
+            if (codeTypeReference.BaseType.Contains("System.Tuple"))
+            {
+                return $"{{{MapCodeTypeReferenceCollectionToTupleTsText(codeTypeReference.TypeArguments)}}}";
+            }
+
+            if (codeTypeReference.TypeArguments.Count > 0)
+            {
+                var genericTypeName = codeTypeReference.BaseType.Substring(0, codeTypeReference.BaseType.IndexOf('`'));
+                return $"{genericTypeName}<{MapCodeTypeReferenceCollectionToTsText(codeTypeReference.TypeArguments)}>";
+            }
+
+            System.Diagnostics.Trace.TraceWarning($"{codeTypeReference.BaseType} is mapped to any.");
+            return "any";
+        }
+
+        /// <summary>
+        /// Get text of CodeTypeReferenceCollection in CSV
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        internal static string MapCodeTypeReferenceCollectionToTsText(CodeTypeReferenceCollection collection)
+        {
+            string[] ss = new string[collection.Count];
+            for (int i = 0; i < collection.Count; i++)
+            {
+                ss[i] = MapCodeTypeReferenceToTsText(collection[i]);
+            }
+            return String.Join(", ", ss);
+        }
+
+        internal static string MapCodeTypeReferenceCollectionToTupleTsText(CodeTypeReferenceCollection collection)
+        {
+            if (collection.Count > 8)
+                throw new ArgumentException("Current supports only up to 8 members for tuple.", "collection");
+            string[] ss = new string[collection.Count];
+            for (int i = 0; i < collection.Count; i++)
+            {
+                var typeName = MapCodeTypeReferenceToTsText(collection[i]);
+                var propertyName = (i < 7) ? "Item" + (i + 1).ToString() : "Rest";
+                ss[i] = (TsCodeGenerationOptions.Instance.CamelCase? Fonlow.Text.StringExtensions.ToCamelCase( propertyName) : propertyName) 
+                    + ":" + typeName;
+            }
+            return String.Join(", ", ss);
+        }
+
+
+    }
+
+
+}

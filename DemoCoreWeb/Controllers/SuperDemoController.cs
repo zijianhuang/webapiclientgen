e9f589b443d1f822084d5b7db60ccdb5d4dae579
Z <zijianhuang2000@gmail.com>
2018-08-09 07:34:49 +1000

    minor

--- DemoCoreWeb/Controllers/SuperDemoController.cs
+++ DemoCoreWeb/Controllers/SuperDemoController.cs
@@ -168,16 +168,12 @@ namespace DemoWebApi.Controllers
 
         [HttpGet]
         [Route("TextStream")]
-        public HttpResponseMessage GetTextStream()
+        public IActionResult GetTextStream()
         {
             var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes("abcdefg"));
             {
                 var content = new StreamContent(stream);
-                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
-                return new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = content
-                };
+                return new FileStreamResult(stream, new Microsoft.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream"));
             }
         }
 

0d3a058affaaa52fa0d524a2a96ad49fce64ec71
Z <zijianhuang2000@gmail.com>
2021-03-18 14:52:23 +1000

    ok

--- Tests/Poco2TsTestsCore/DocCommentTests.cs
+++ Tests/Poco2TsTestsCore/DocCommentTests.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Fonlow.DocComment;
 using Xunit;
-using Fonlow.DocComment;
 
 namespace Poco2TsTests
 {
-    public class DocCommentTest
-    {
-        [Fact]
-        public void TestReadDocComment()
-        {
-            var d = DocCommentLookup.Create(@"C:\VsProjects\webapiclientgen\DemoCoreWeb\bin\Debug\net5.0\DemoCoreWeb.xml");
-            Assert.NotNull(d);
-            Assert.Equal("DemoCoreWeb", d.XmlDoc.assembly.name);
-            var summary = d.GetMember("T:DemoWebApi.Controllers.HeroesController").summary;
+	public class DocCommentTest
+	{
+		[Fact]
+		public void TestReadDocComment()
+		{
+			var d = DocCommentLookup.Create(@"C:\VsProjects\OpenSource\webapiclientgen\DemoCoreWeb\bin\Debug\net5.0\DemoCoreWeb.xml");
+			Assert.NotNull(d);
+			Assert.Equal("DemoCoreWeb", d.XmlDoc.assembly.name);
+			var summary = d.GetMember("T:DemoWebApi.Controllers.HeroesController").summary;
 
-            const string expected = "\n            Heroes operations\n            ";
-            Assert.Equal(expected, summary.Text[0]);
+			const string expected = "\n            Heroes operations\n            ";
+			Assert.Equal(expected, summary.Text[0]);
 
-        }
+		}
 
-        [Fact]
-        public void TestReadNotExist()
-        {
-            var r = DocCommentLookup.Create(@"C:\VsProjects\webapiclientgen\NotExist\App_Data\xmlDocument.xml");
-            Assert.Null(r);
-        }
+		[Fact]
+		public void TestReadNotExist()
+		{
+			var r = DocCommentLookup.Create(@"C:\VsProjects\webapiclientgen\NotExist\App_Data\xmlDocument.xml");
+			Assert.Null(r);
+		}
 
-    }
+	}
 }

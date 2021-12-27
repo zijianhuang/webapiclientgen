245f1d1b20dcc70f9ea64f6b1f11ac586fc8ada1
Z <zijianhuang2000@gmail.com>
2021-12-27 13:49:52 +1000

    updated .net 6

--- CreateAspNetCoreClientApi.ps1
+++ CreateAspNetCoreClientApi.ps1
@@ -1,7 +1,7 @@
 cd $PSScriptRoot
 #Make sure CodeGen.json is saved in format ANSI or UTF-8 without BOM, since ASP.NET Core 2.0 Web API will fail to deserialize POST Body that contains BOM.
 #Step 1: Launch Website
-$path = "$PSScriptRoot\DemoCoreWeb\bin\Debug\net5.0"
+$path = "$PSScriptRoot\DemoCoreWeb\bin\Debug\net6.0"
 $procArgs = @{
     FilePath         = "dotnet.exe"
     ArgumentList     = "$path\DemoCoreWeb.dll"

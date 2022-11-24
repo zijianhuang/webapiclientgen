39775d8dab86d0ea6fb2a519434f98f3dab44b14
Z <zijianhuang2000@gmail.com>
2022-11-24 10:17:47 +1000

    ok

--- HeroesDemo/startDev.bat
+++ HeroesDemo/startDev.bat
@@ -1,4 +1,4 @@
 ::Build for testing with dotnet hosting
 ::set %current%=%~dp0
 
-call "c:\green\SimpleHost\SimpleHost.exe" C:\VSProjects\Fonlow\PoemsCollection\ngdist\poemsng http://localhost:5200
+call "c:\green\SimpleHost\SimpleHost.exe" C:\VSProjects\OpenSource\webapiclientgen\ngdist\dev http://localhost:5200

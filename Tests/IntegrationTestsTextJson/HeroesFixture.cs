0694295ffadde414f4d53a1de6010e19042b12a6
Zijian <161275903+ZijianFLG@users.noreply.github.com>
2024-04-09 06:01:31 +1000

    investigate

---
+++ Tests/IntegrationTestsTextJson/HeroesFixture.cs
@@ -0,0 +1,24 @@
+﻿using Fonlow.Testing;
+
+namespace IntegrationTests
+{
+	public class HeroesFixture : DefaultHttpClient
+	{
+		public HeroesFixture()
+		{
+			var jsonSerializerSettings = new System.Text.Json.JsonSerializerOptions()
+			{
+				DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault,
+				PropertyNameCaseInsensitive = true,
+			};
+
+			//jsonSerializerSettings.Converters.Add(new DateOnlyJsonConverter());
+			//jsonSerializerSettings.Converters.Add(new DateOnlyNullableJsonConverter());
+			Api = new DemoWebApi.Controllers.Client.Heroes(HttpClient, jsonSerializerSettings);
+		}
+
+		public DemoWebApi.Controllers.Client.Heroes Api { get; private set; }
+	}
+
+
+}

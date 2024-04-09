80731642c992c53807bdec496ea0d02da5188149
Zijian <161275903+ZijianFLG@users.noreply.github.com>
2024-04-10 06:22:47 +1000

    ok

--- DemoCoreWeb/Program.cs
+++ DemoCoreWeb/Program.cs
@@ -1,25 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Fonlow.IntegralExtensions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System;
 
-namespace DemoCoreWeb
+System.Reflection.Assembly appAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+string dirOfAppAssembly = System.IO.Path.GetDirectoryName(appAssembly.Location);
+IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile(System.IO.Path.Combine(dirOfAppAssembly, "appsettings.json")).Build();
+var appSettings = config.GetSection("appSettings");
+var environment = appSettings.GetValue<string>("environment");
+
+string webRootPath = "./";
+
+// WebRootPath is to tell the Web server where to look for files to serve.
+// ContentRootPath is to tell the Web service code where to look for data.
+// On Windows, ContentRootPath is the starting folder of the app assembly, and on MacOS, it is the user profile folder like //Users/MyName.
+// Thus on MacOS, the App_Data folder should be under the user profile folder.
+var options = new WebApplicationOptions
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+	WebRootPath = webRootPath,
+	Args = args,
+};
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+
+var builder = WebApplication.CreateBuilder(options);
+builder.Configuration.AddConfiguration(config);
+Console.WriteLine($"Start at contentRootPath: {builder.Environment.ContentRootPath}; WebRootPath: {builder.Environment.WebRootPath}");
+
+builder.Services.AddControllers(configure =>
+{
+#if DEBUG
+	configure.Conventions.Add(new Fonlow.CodeDom.Web.ApiExplorerVisibilityEnabledConvention());//To make ApiExplorer be visible to WebApiClientGen
+#endif
+	configure.Filters.Add(new DemoCoreWeb.ValidateModelAttribute());
+
+})
+.AddNewtonsoftJson(
+	options =>
+	{
+		options.SerializerSettings.DateParseHandling = Newtonsoft.Json.DateParseHandling.DateTimeOffset; //Better with this for cross-timezone minValue and .NET Framework clients.
+		options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore; //So when controller will ignore null fileds when returing data
+
+		//options.SerializerSettings.Converters.Add(new DateOnlyJsonConverter()); //not needed for ASP.NET 7 and .NET 7 clients. However .NET 6 clients and .NET Framework clients still need DateOnlyJsonConverter
+		//options.SerializerSettings.Converters.Add(new DateOnlyNullableJsonConverter()); // also, needed by JavaScript clients.
+
+		// JS clients need these integral JsonConverters for large integral numbers
+		options.SerializerSettings.Converters.Add(new Int64JsonConverter());
+		options.SerializerSettings.Converters.Add(new Int64NullableJsonConverter());
+		options.SerializerSettings.Converters.Add(new UInt64JsonConverter());
+		options.SerializerSettings.Converters.Add(new UInt64NullableJsonConverter());
+		options.SerializerSettings.Converters.Add(new BigIntegerJsonConverter());
+		options.SerializerSettings.Converters.Add(new BigIntegerNullableJsonConverter());
+	}
+);
+
+builder.Services.AddCors(options => options.AddPolicy("All", builder =>
+{
+	builder.AllowAnyMethod()
+		   .AllowAnyOrigin()
+		   .AllowAnyHeader()
+		   ;
+}));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) //ASPNETCORE_ENVIRONMENT=Development in web.config
+{
+	app.UseDeveloperExceptionPage();
 }
+else
+{
+	//	//Only release build support https redirection.
+	//#if RELEASE
+	//	if (useHttps) // for locally running app, no need to have https.
+	//	{
+	//		app.UseHttpsRedirection();
+	//		app.UseHsts();//https://learn.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-6.0
+	//	}
+	//#endif
+}
+app.UseRouting();
+app.UseCors(builder => builder.AllowAnyOrigin()
+	.AllowAnyHeader().AllowAnyMethod()
+	);
+app.MapControllers();
+
+if (args.Length > 1)
+{
+	app.Urls.Add(builder.Environment.WebRootPath);
+}
+
+#if DEBUG  // This is for running the QUnit cases with tests.html. The CodeGenSetting should be "TypeScriptJQFolder": "..\\..\\..\\Scripts\\ClientApi" without StaticFiles
+app.UseStaticFiles(new StaticFileOptions
+{
+	OnPrepareResponse = ctx =>
+	{
+		ctx.Context.Response.Headers.Add(
+			 "Cache-Control", "no-cache");
+	}
+}); //"TypeScriptJQFolder": "..\\..\\..\\..\\Scripts\\ClientApi" because of wwwwroot in play
+#endif
+
+app.Run();
+Console.WriteLine("Run Done.");
--- DemoCoreWeb/Startup.cs
+++ DemoCoreWeb/Startup.cs
@@ -1,98 +1,82 @@
-﻿using Fonlow.DateOnlyExtensions;
-using Fonlow.IntegralExtensions;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-namespace DemoCoreWeb
-{
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+﻿//using Fonlow.DateOnlyExtensions;
+//using Fonlow.IntegralExtensions;
+//namespace DemoCoreWeb
+//{
+//	public class Startup
+//	{
+//		public Startup(IConfiguration configuration)
+//		{
+//			Configuration = configuration;
+//		}
 
-		public IConfiguration Configuration { get; }
+//		public IConfiguration Configuration { get; }
 
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddControllers(
-				options =>
-				{
-					options.Filters.Add(new ValidateModelAttribute()); // wholesale style to check model binding for all API calls.
-					//options.OutputFormatters by default includes: HttpNoContent, String, Stream, SystemTextJson
-#if DEBUG
-					options.Conventions.Add(new Fonlow.CodeDom.Web.ApiExplorerVisibilityEnabledConvention());//To make ApiExplorer be visible to WebApiClientGen
+//		public void ConfigureServices(IServiceCollection services)
+//		{
+//			services.AddControllers(
+//				options =>
+//				{
+//					options.Filters.Add(new ValidateModelAttribute()); // wholesale style to check model binding for all API calls.
+//					//options.OutputFormatters by default includes: HttpNoContent, String, Stream, SystemTextJson
+//#if DEBUG
+//					options.Conventions.Add(new Fonlow.CodeDom.Web.ApiExplorerVisibilityEnabledConvention());//To make ApiExplorer be visible to WebApiClientGen
 
-#endif
-				}
-			)
-			//.AddJsonOptions(// as of .NET 7/8, could not handle JS/CS test cases getInt2D, postInt2D and PostDictionaryOfPeople, around 14 C# test cases fail.
-			//options =>
-			//{
-			//	options.JsonSerializerOptions.Converters.Add(new Fonlow.Text.Json.DateOnlyExtensions.DateOnlyJsonConverter()); //needed by JS clients
-			//	options.JsonSerializerOptions.Converters.Add(new Fonlow.Text.Json.DateOnlyExtensions.DateOnlyNullableJsonConverter());
-			//	//options.JsonSerializerOptions.Converters.Add(new Fonlow.Text.Json.DateOnlyExtensions.DateTimeJsonConverter()); // needed by only .NET Framework clients
-			//	//options.JsonSerializerOptions.Converters.Add(new Fonlow.Text.Json.DateOnlyExtensions.DateTimeNullableJsonConverter());
-			//	//options.JsonSerializerOptions.Converters.Add(new Fonlow.Text.Json.DateOnlyExtensions.DateTimeOffsetJsonConverter()); // needed by only .NET Framework clients
-			//	//options.JsonSerializerOptions.Converters.Add(new Fonlow.Text.Json.DateOnlyExtensions.DateTimeOffsetNullableJsonConverter());
+//#endif
+//				}
+//			)
+//			.AddNewtonsoftJson(
+//				options =>
+//				{
+//					options.SerializerSettings.DateParseHandling = Newtonsoft.Json.DateParseHandling.DateTimeOffset; //Better with this for cross-timezone minValue and .NET Framework clients.
+//					options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore; //So when controller will ignore null fileds when returing data
 
-			//});
-			.AddNewtonsoftJson(
-				options =>
-				{
-					options.SerializerSettings.DateParseHandling = Newtonsoft.Json.DateParseHandling.DateTimeOffset; //Better with this for cross-timezone minValue and .NET Framework clients.
-					options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore; //So when controller will ignore null fileds when returing data
+//					options.SerializerSettings.Converters.Add(new DateOnlyJsonConverter()); //not needed for ASP.NET 7 and .NET 7 clients. However .NET 6 clients and .NET Framework clients still need DateOnlyJsonConverter
+//					options.SerializerSettings.Converters.Add(new DateOnlyNullableJsonConverter()); // also, needed by JavaScript clients.
 
-					options.SerializerSettings.Converters.Add(new DateOnlyJsonConverter()); //not needed for ASP.NET 7 and .NET 7 clients. However .NET 6 clients and .NET Framework clients still need DateOnlyJsonConverter
-					options.SerializerSettings.Converters.Add(new DateOnlyNullableJsonConverter()); // also, needed by JavaScript clients.
+//					// JS clients need these integral JsonConverters for large integral numbers
+//					options.SerializerSettings.Converters.Add(new Int64JsonConverter());
+//					options.SerializerSettings.Converters.Add(new Int64NullableJsonConverter());
+//					options.SerializerSettings.Converters.Add(new UInt64JsonConverter());
+//					options.SerializerSettings.Converters.Add(new UInt64NullableJsonConverter());
+//					options.SerializerSettings.Converters.Add(new BigIntegerJsonConverter());
+//					options.SerializerSettings.Converters.Add(new BigIntegerNullableJsonConverter());
+//				}
+//			)
+//			.ConfigureApiBehaviorOptions(options=>{
+//				//options.SuppressModelStateInvalidFilter = true; //No effect on the API designs here which rarely return BadRequestObjectResult directly.
+//			});
 
-					// JS clients need these integral JsonConverters for large integral numbers
-					options.SerializerSettings.Converters.Add(new Int64JsonConverter());
-					options.SerializerSettings.Converters.Add(new Int64NullableJsonConverter());
-					options.SerializerSettings.Converters.Add(new UInt64JsonConverter());
-					options.SerializerSettings.Converters.Add(new UInt64NullableJsonConverter());
-					options.SerializerSettings.Converters.Add(new BigIntegerJsonConverter());
-					options.SerializerSettings.Converters.Add(new BigIntegerNullableJsonConverter());
-				}
-			)
-			.ConfigureApiBehaviorOptions(options=>{
-				//options.SuppressModelStateInvalidFilter = true; //No effect on the API designs here which rarely return BadRequestObjectResult directly.
-			});
+//			services.AddRouting();
+//			services.AddCors();
+//		}
 
-			services.AddRouting();
-			services.AddCors();
-		}
+//		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//		{
+//			if (env.IsDevelopment())
+//			{
+//				app.UseDeveloperExceptionPage();
+//			}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+//			app.UseRouting();
+//			app.UseCors(builder => builder.AllowAnyOrigin()
+//				.AllowAnyHeader().AllowAnyMethod()
+//				);
+//			app.UseEndpoints(endpoints =>
+//			{
+//				endpoints.MapControllers();
+//			});
 
-			app.UseRouting();
-			app.UseCors(builder => builder.AllowAnyOrigin()
-				.AllowAnyHeader().AllowAnyMethod()
-				);
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
-
-#if DEBUG  // This is for running the QUnit cases with tests.html. The CodeGenSetting should be "TypeScriptJQFolder": "..\\..\\..\\Scripts\\ClientApi" without StaticFiles
-			app.UseStaticFiles(new StaticFileOptions
-			{
-				OnPrepareResponse = ctx =>
-				{
-					ctx.Context.Response.Headers.Add(
-						 "Cache-Control", "no-cache");
-				}
-			}); //"TypeScriptJQFolder": "..\\..\\..\\..\\Scripts\\ClientApi" because of wwwwroot in play
-#endif
-		}
-	}
-}
+//#if DEBUG  // This is for running the QUnit cases with tests.html. The CodeGenSetting should be "TypeScriptJQFolder": "..\\..\\..\\Scripts\\ClientApi" without StaticFiles
+//			app.UseStaticFiles(new StaticFileOptions
+//			{
+//				OnPrepareResponse = ctx =>
+//				{
+//					ctx.Context.Response.Headers.Add(
+//						 "Cache-Control", "no-cache");
+//				}
+//			}); //"TypeScriptJQFolder": "..\\..\\..\\..\\Scripts\\ClientApi" because of wwwwroot in play
+//#endif
+//		}
+//	}
+//}

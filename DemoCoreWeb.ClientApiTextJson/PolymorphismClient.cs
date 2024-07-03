97af50dc4707d493646064a7924848145fb65f82
Zijian <zijianhuang2000@gmail.com>
2024-07-03 10:44:59 +1000

    ok

--- DemoCoreWeb.ClientApiTextJson/PolymorphismClient.cs
+++ DemoCoreWeb.ClientApiTextJson/PolymorphismClient.cs
@@ -35,8 +35,8 @@ namespace DemoCoreWeb.ClientApiTextJson
 			var pairs = new KeyValuePair<string, string>[]
 						{
 							new KeyValuePair<string, string>( "grant_type", model.GrantType ),
-							//new KeyValuePair<string, string>( "username", model.Username ),
-							//new KeyValuePair<string, string> ( "password", model.Password )
+							new KeyValuePair<string, string>( "username", model.Username ),
+							new KeyValuePair<string, string> ( "password", model.Password )
 						};
 			var content = new FormUrlEncodedContent(pairs);
 			httpRequestMessage.Content = content;

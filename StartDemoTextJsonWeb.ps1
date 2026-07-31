65f2c4bca8e20b1a67b8305768538c0440b117c6
Z <zijianhuang2000@gmail.com>
2026-08-01 09:25:43 +1000

    take full advantages of Parallelization(Mode = ParallelMode.All , https://github.com/xunit/xunit/issues/2055

--- StartDemoTextJsonWeb.ps1
+++ StartDemoTextJsonWeb.ps1
@@ -3,11 +3,11 @@ Set-Location $PSScriptRoot
 $path = "$PSScriptRoot/DemoTextJsonWeb"
 $procArgs = @{
     FilePath         = "dotnet.exe"
-    ArgumentList     = "run --project $path/DemoTextJsonWeb.csproj --no-build --launch-profile CodeGen"
+    ArgumentList     = "run --project $path/DemoTextJsonWeb.csproj --no-build --launch-profile http"
     WorkingDirectory = $path
     PassThru         = $true
 }
 $process = Start-Process @procArgs
 
-Invoke-RestMethod http://localhost:5000/api/values -Method GET
+Invoke-RestMethod http://localhost:6000/api/values -Method GET
 
